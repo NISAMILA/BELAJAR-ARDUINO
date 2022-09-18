5c7de1673536c32835ae3c8df91c30a7da1e8599
NISAMILA <111625939+NISAMILA@users.noreply.github.com>
2022-09-18 03:37:37 -0700

    Add files via upload

---
+++ BELAJAR_LAMPU_LALU_LINTAS.ino
@@ -0,0 +1,56 @@
+int LED1 = 13;
+int LED2 = 12;
+int LED3 = 11;
+int LED5 = 10;
+int LED5 = 9;
+
+
+void setup() {
+  // put your setup code here, to run once:
+ pinMode (LED, OUTPUT);
+ pinMode (LED, OUTPUT);
+ pinMode (LED, OUTPUT);
+ pinMode (LED, OUTPUT);
+ pinMode (LED, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+ digitalWrite(LED1, HIGH); // turn on LED1
+ delay(200);               // wait for 200ms
+ digitalWrite(LED2, HIGH); // turn on LED2
+ delay(20);                // wait for 200ms
+ digitalWrite(LED3, HIGH); // turn on LED3
+ delay(200);               // wait for 200ms
+ digitalWrite(LED4, HIGH); // turn on LED4
+ delay(200);               // wait for 200ms
+ digitalWrite(LED5, HIGH); // turn on LED5
+ delay(200);               // wait for 200ms
+ digitalWrite(LED1, LOW);  // turn on LED1
+ delay(120);               // wait for 300ms
+ digitalWrite(LED2, HIGH); // turn on LED2
+ delay(200);               // wait for 200ms
+ digitalWrite(LED3, HIGH); // turn on LED3
+ delay(100);               // wait for 200ms
+ digitalWrite(LED4, LOW);  // turn on LED4
+ delay(120);               // wait for 300ms
+ digitalWrite(LED5, HIGH); // turn on LED5
+ delay(200);      
+ 
+
+ 
+
+ 
+
+ 
+
+ 
+
+ 
+
+
+
+
+
+  
+}
